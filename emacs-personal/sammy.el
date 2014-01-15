35165fb38bf3f5c422afd57b7ca6e6fd38314b43
Samuel Schuhmacher <sschuhmacher@synapticon.com>
2014-01-15 15:30:57 +0100

    Add inline Elisp eval and replacing.

--- emacs-personal/sammy.el
+++ emacs-personal/sammy.el
@@ -80,5 +80,21 @@
 ;;;
 (custom-set-variables '(coffee-tab-width 2))
 
+;;;
+;;; Inline Elisp
+;;;
+
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
+(global-set-key (kbd "C-x C-e") 'eval-and-replace)
+
 (provide 'sammy)
 ;;; sammy.el ends here
