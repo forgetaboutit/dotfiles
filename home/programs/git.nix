32977af3eacd4de2f2ac1c17a206979a3666fade
Samuel Schuhmacher <post@samuel-schuhmacher.de>
2026-06-07 23:33:04 +0200

    feat: Extend Git configuration

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -3,38 +3,14 @@
     core = {
       editor = "nvim";
       pager = "diff-so-fancy | less --tabs=4 --quit-if-one-screen --RAW-CONTROL-CHARS --no-init";
+      fsmonitor = true;
+      untrackedCache = true;
     };
 
-    init.defaultBranch = "main";
-
-    color.ui = true;
-    # Always run fetch with `--prune`
-    fetch.prune = true;
-    pull.rebase = true;
-
-    push = {
-      # Always push branch to remote branch with the same name
-      default = "current";
-      autoSetupRemote = true;
+    user = {
+      name = "Samuel Schuhmacher";
+      email = "post@samuel-schuhmacher.de";
     };
-
-    branch = {
-      # Automatically track the starting branch
-      autosetupmerge = "always";
-      # Always perform a rebase on pull, not a merge
-      autosetuprebase = "always";
-    };
-  };
-in {
-  home.packages = with pkgs.gitAndTools; [
-    diff-so-fancy # diff with colors
-    tig # diff and commit view
-  ];
-
-  programs.git = {
-    enable = true;
-    userName = "Samuel Schuhmacher";
-    userEmail = "post@samuel-schuhmacher.de";
 
     aliases = {
       co = "checkout";
@@ -48,10 +24,69 @@ in {
       f = "fetch --all --tags --prune --prune-tags --force";
     };
 
+    column.ui = "auto";
+    color.ui = true;
+    tag.sort = "version:refname";
+    init.defaultBranch = "main";
+    diff = {
+      algorithm = "histogram";
+      colorMoved = "plain";
+      mnemonicPrefix = true;
+      renames = true;
+    };
+
+    # Always run fetch with `--prune`
+    fetch = {
+      prune = true;
+      pruneTags = true;
+      all = true;
+    };
+
+    pull = {
+      rebase = true;
+    };
+
+    rebase = {
+      abbreviateCommands = true;
+      autoSquash = true;
+      autoStash = true;
+      updateRefs = true;
+    };
+
+    rerere = {
+      enabled = true;
+      autoupdate = true;
+    };
+
+    push = {
+      # Always push branch to remote branch with the same name
+      default = "current";
+      autoSetupRemote = true;
+      followTags = true;
+    };
+
+    branch = {
+      sort = "-committerdate";
+      # Automatically track the starting branch
+      autosetupmerge = "always";
+      # Always perform a rebase on pull, not a merge
+      autosetuprebase = "always";
+    };
+  };
+in {
+  home.packages = with pkgs; [
+    git
+    diff-so-fancy # diff with colors
+    tig # diff and commit view
+  ];
+
+  programs.git = {
+    enable = true;
+
     signing = {
       key = "441CA6ABF67CF1F78B7D789FAF6DF102AF67F697";
     };
 
-    extraConfig = gitConfig;
+    settings = gitConfig;
   };
 }
